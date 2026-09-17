260d5e9fac43953eb50f615d251052a671d46d02
Ayoade Naheem <ayoadenaheem@gmail.com>
2026-09-17 16:28:21 +0100

    Initial commit for Voting Eligibility

---
+++ script.js
@@ -0,0 +1,21 @@
+ function checkEligibility(){
+        let userDate = document.getElementById('userInput').value;
+        let displayArea =  document.getElementById('show');
+        if(userDate == ''){
+          alert('input should not be empty')
+      }else {
+        let birthDate = Number(userDate);
+        let currentYear = new Date().getFullYear()
+        let age = currentYear - birthDate;
+        if(age >= 18){
+          // console.log('vote dey go jor');
+          displayArea.innerHTML = `<p class="text-success" >You are eligible to vote, carry on</p>`
+          
+        }else{
+          console.log('grow up');
+          displayArea.innerHTML = `<p>you are not eligible to vote, you have ${18-age} years left to vote </p>`
+          
+        }
+
+      }
+    }
